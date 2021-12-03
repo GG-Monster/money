d4b5eb30b4b29e6a8b00da2ff69c1cdb2355f3e6
lishu <lilili925@icloud.com>
2021-12-03 17:08:41 +0800

    money 页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-      <router-link to="/money">钱包 ｜</router-link>
-      <router-link to="/labels">标签 ｜</router-link>
-      <router-link to="/statistics">统计 ｜</router-link>
-    </div>
+  <div class="nav">
+    <router-link to="/money">钱包 ｜</router-link>
+    <router-link to="/labels">标签 ｜</router-link>
+    <router-link to="/statistics">统计 ｜</router-link>
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,5 +13,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav {
+  border: 1px solid red;
+}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,9 @@
 <template>
-<div>
-  money.vue
-  <Nav />
+<div class="nav-wrapper">
+  <div class="content">
+    <p>money.vue</p>
+  </div>
+  <Nav/>
 </div>
 </template>
 
@@ -12,5 +14,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.content{
+  border: 1px solid blue;
+overflow: auto;
+flex-grow: 1;
+}
+.nav-wrapper{
+border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 </style>
