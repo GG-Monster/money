571766f39e91953e2712e37199d1800d6102e6d3
lishu <lilili925@icloud.com>
2021-12-03 15:43:12 +0800

    创建三个页面

---
+++ src/router/index.ts
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter, {RouteConfig} from 'vue-router'
+import Money from "@/views/Money.vue";
+import Labels from "@/views/Labels.vue";
+import Statistics from "@/views/Statistics.vue";
+
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/money',
+        component: Money
+    },
+    {
+        path: '/',
+        redirect: '/money'
+    },
+    {
+        path: '/labels',
+        component: Labels
+    },
+    {
+        path: '/statistics',
+        component: Statistics
+    },
+    // {
+    //     path: '/about',
+    //     name: 'About',
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // }
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
