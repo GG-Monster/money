9a0f80803bf6da4f779bb1ccf00b932f8d7b0fce
lishu <lilili925@icloud.com>
2021-12-04 18:20:11 +0800

    撤销删一条 恢复

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 // module.exports = {
 //   lintOnSave: false
-const path=require('path');
+const path = require('path');
 // Require statement not part of import statement.(@typescript-eslint/no-var-requires)
 //解决办法.eslintrc.js中的 rules 属性新增以下内容：'@typescript-eslint/no-var-requires': 0
-module.exports= {
+module.exports = {
     lintOnSave: false,
     chainWebpack: config => {
         const dir = path.resolve(__dirname, 'src/assets/icons')
@@ -12,8 +12,8 @@ module.exports= {
             .test(/\.svg$/)
             .include.add(dir).end()
             .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
-            .use('svgo-loader').loader('svgo-loader')
-            .tap(options=>({...options,plugins:[{removeAttrs:{attrs:'fill'}}]})).end()
+            // .use('svgo-loader').loader('svgo-loader')
+        // .tap(options=>({...options,plugins:[{removeAttrs:{attrs:'fill'}}]})).end()
         config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
         config.module.rule('svg').exclude.add(dir)
 
