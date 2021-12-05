cf45beb5b684c99114fdbbd8322664e38cb2b845
lishu <lilili925@icloud.com>
2021-12-06 01:39:18 +0800

    添加css样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -8,13 +8,13 @@
         <li>行</li>
     </ul>
     <div class="new">
-      <button>新增标签</button>
+      <button> +新增标签 </button>
     </div>
     </div>
     <div>
       <label class="notes">
         <span class="name">备注</span>
-        <input type="text">
+        <input type="text" placeholder="在这里输入备注">
       </label>
     </div>
     <div>
@@ -53,4 +53,80 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+@import "~@/assets/styles/global.scss";
+@import "~@/assets/styles/reset.scss";
+.types{
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+
+  > li{
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    //&表示当前选择器 而非当前的子选择器
+    &.selected::after{
+      content:'';
+      position: absolute;
+      bottom:0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+.tags{
+  font-size: 14px;
+  padding: 16px;
+  > .current{
+    display: flex;
+    > li{
+      background: #d9d9d9;
+      $h:24px;
+      height:$h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 16px;
+      margin-right: 12px;
+
+    }
+  }
+  > .new{
+    padding-top: 16px;
+    button{
+background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+.notes{
+  background: #f5f5f5;
+  font-size: 14px;
+  display: flex;
+  padding-left: 16px;
+  align-items: center;
+.name{
+  padding-right: 16px;
+}
+  input{
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    padding-right: 16px;
+    border: 0px;
+
+  }
+}
+
+
+
+
 </style>
