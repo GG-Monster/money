ba2905eacebeca3b33dac0db351dbb2de1a57e0e
lishu <lilili925@icloud.com>
2021-12-07 22:40:20 +0800

    封装model

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -57,7 +57,8 @@ this.output='0';
 }
 ok(){
     this.$emit('update:value',this.output);
-    this.$emit('submit',this.output)
+    this.$emit('submit',this.output);
+    this.output='0';
 }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -13,20 +13,16 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Vue from "vue";
-import {Component, Prop,Watch} from "vue-property-decorator";
-type Record={
-  tags:string[],
-  notes:string,
-  type:string,
-  amount:number
-}
+import {Component,Watch} from "vue-property-decorator";
+import model from "@/model";
+const recordList=model.fetch();
 @Component(
     {components:{Tags,Notes,Types,NumberPad}}
 )
 export default class Money extends Vue {
   tags=["衣","食","住","行"];
-  recordList:Record[]=[];
-  record:Record={
+  recordList:RecordItem[]=recordList;
+  record:RecordItem={
     tags:[],
     notes:'',
     type:'-',
@@ -42,13 +38,13 @@ this.record.amount=parseFloat(value);
     this.record.tags=value;
   }
   saveRecord(){
-    const record2=JSON.parse(JSON.stringify(this.record));
+    const record2=model.clone(this.record);
+    record2.createdTime=new Date();
 this.recordList.push(record2);
-    console.log(this.recordList);
   }
   @Watch('recordList')
   onRecordListChange(){
-    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
+  model.save(this.recordList);
   }
 }
 </script>
