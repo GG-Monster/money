1e33de59ca55b479e5e3512f53fdadfe8b1d54b4
lishu <lilili925@icloud.com>
2021-12-07 23:49:58 +0800

    label组件的html和部分css

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,8 +1,65 @@
 <template>
-<Layout>labels.vue</Layout>
+<Layout>
+  <ol class="tags">
+    <li>
+      <span>衣</span>
+      <Icon name="right"/>
+    </li>
+    <li>
+      <span>食</span>
+      <Icon name="right"/>
+
+    </li>
+    <li>
+      <span>住</span>
+      <Icon name="right"/>
+
+    </li>
+    <li>
+      <span>行</span>
+      <Icon name="right"/>
+    </li>
+  </ol>
+  <div class="createTag-container"><button class="createTag">新建标签</button></div>
+</Layout>
 </template>
 <script lang="ts">
+
 export default {
   name: "Labels",
 }
 </script>
+<style lang="scss" scoped>
+.tags{
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+  >li{
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    >svg{
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+.createTag{
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+&-container{
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+}
+}
+
+</style>
