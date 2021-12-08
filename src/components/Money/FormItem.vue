cc7455f30fea1eadda7acfac6f2d58bbee772b04
lishu <lilili925@icloud.com>
2021-12-09 00:27:55 +0800

    修复value(prop)被赋值的bug

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -3,7 +3,8 @@
     <label class="formItem">
       <span class="name">{{ this.fieldName }}</span>
       <input type="text"
-             v-model="value"
+             :value="value"
+             @input="onValueChanged($event.target.value)"
              :placeholder="this.placeholder">
     </label>
   </div>
