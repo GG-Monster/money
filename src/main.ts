377aea8e417d7be20fc0117874e9cdda1301f566
lishu <lilili925@icloud.com>
2022-01-08 00:23:29 +0800

    电脑浏览时可手机扫描二维码浏览

--- src/main.ts
+++ src/main.ts
@@ -18,18 +18,18 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
-// window.onload=function(){
-//   setTimeout(function(){
-//     window.scrollTo(9,200);
-//   },0);
-// };
-// if (document.documentElement.clientWidth>500){
-//   window.alert('为保证浏览效果，请使用手机打开页面！');
-//   const img=document.createElement('img');
-//   img.src='/qrcode.png';
-//   img.style.position='fixed';
-//   img.style.left='50%';
-//   img.style.top='50%';
-//   img.style.transform='0 0 10px rgba(0,0,0,0.25)';
-//   document.body.appendChild(img);
-// }
+window.onload=function(){
+  setTimeout(function(){
+    window.scrollTo(9,200);
+  },0);
+};
+if (document.documentElement.clientWidth>500){
+  window.alert('为保证浏览效果，请使用手机打开页面！');
+  const img=document.createElement('img');
+  img.src='/qrcode.png';
+  img.style.position='fixed';
+  img.style.left='50%';
+  img.style.top='50%';
+  img.style.transform='translate(-50%,-50%)';
+  document.body.appendChild(img);
+}
