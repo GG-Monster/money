bcdb82bf0ad59249439e7ec6956900bc3094c394
lishu <lilili925@icloud.com>
2022-01-07 23:59:21 +0800

    $h bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -60,7 +60,7 @@ export default class Tags extends mixins(TagHelper) {
       $h: 24px;
       height: $h;
       line-height: $h;
-      border-radius: $h/2;
+      border-radius: 12px;
       padding: 0 16px;
       margin-right: 12px;
       margin-top: 4px;
